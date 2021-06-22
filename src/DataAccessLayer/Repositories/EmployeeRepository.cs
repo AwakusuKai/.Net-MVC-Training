3a7010af382e959a27b0f09bceabed137875a704
Vaskovich, Vladislav <vvaskovich@qulix.com>
2021-06-22 12:12:30 +0300

    Removed extra code

--- src/DataAccessLayer/Repositories/EmployeeRepository.cs
+++ src/DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -31,103 +31,25 @@ namespace DataAccessLayer.Repositories
         public IEnumerable<Employee> GetAll()
         {
             return SQLCall.GetAllRequest<Employee>(connectionString, "spGetEmployees");
-            /*List<Employee> employees = new List<Employee>();
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand("spGetEmployees", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var employee = new Employee()
-                    {
-                        Id = Convert.ToInt32(rdr["Id"]),
-                        Name = rdr["Name"].ToString(),
-                        Surname = rdr["Surname"].ToString(),
-                        MiddleName = rdr["Surname"].ToString(),
-                        Position = rdr["Position"].ToString()
-                    };
-                    employees.Add(employee);
-                }
-            }
-            return employees;*/
-
         }
 
         public void Create(Employee employee)
         {
             SQLCall.CreateRequest<Employee>(connectionString, "spCreateEmployee", employee);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spCreateEmployee", con);
-                con.Open();
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Name", employee.Name);
-                cmd.Parameters.AddWithValue("@Surname", employee.Surname);
-                cmd.Parameters.AddWithValue("@MiddleName", employee.MiddleName);
-                cmd.Parameters.AddWithValue("@Position", employee.Position);
-                cmd.ExecuteNonQuery();
-            }*/
         }
 
         public Employee GetById(int id)
         {
             return SQLCall.GetByIdRequest<Employee>(connectionString, "spGetEmployeeById", id);
-            /*Employee employee = new Employee();
-            bool isFind = false;
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand("spGetEmployeeById", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Id", id);
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    isFind = true;
-                    employee.Id = Convert.ToInt32(rdr["Id"]);
-                    employee.Name = rdr["Name"].ToString();
-                    employee.Surname = rdr["Surname"].ToString();
-                    employee.MiddleName = rdr["MiddleName"].ToString();
-                    employee.Position = rdr["Position"].ToString();
-                }
-                if (isFind)
-                {
-                    return employee;
-                }
-                return null;
-
-            }*/
         }
 
         public void Update(Employee employee)
         {
             SQLCall.UpdateRequest<Employee>(connectionString, "spUpdateEmployee", employee);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spUpdateEmployee", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@Id", employee.Id);
-                cmd.Parameters.AddWithValue("@Name", employee.Name);
-                cmd.Parameters.AddWithValue("@Surname", employee.Surname);
-                cmd.Parameters.AddWithValue("@MiddleName", employee.MiddleName);
-                cmd.Parameters.AddWithValue("@Position", employee.Position);
-                cmd.ExecuteNonQuery();
-            }*/
         }
         public void Delete(int id)
         {
             SQLCall.DeleteRequest(connectionString, "spDeleteEmployeeById", id);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spDeleteEmployeeById", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@Id", id);
-                cmd.ExecuteNonQuery();
-            }*/
         }
 
     }
--- src/DataAccessLayer/Repositories/ProjectRepository.cs
+++ src/DataAccessLayer/Repositories/ProjectRepository.cs
@@ -28,99 +28,25 @@ namespace DataAccessLayer.Repositories
         public IEnumerable<Project> GetAll()
         {
             return SQLCall.GetAllRequest<Project>(connectionString, "spGetProjects");
-            /*List<Project> projects = new List<Project>();
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand("spGetProjects", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var project = new Project()
-                    {
-                        Id = Convert.ToInt32(rdr["Id"]),
-                        Name = rdr["Name"].ToString(),
-                        ShortName = rdr["ShortName"].ToString(),
-                        Description = rdr["Description"].ToString()
-                    };
-                    projects.Add(project);
-                }
-            }
-            return projects;*/
-
         }
 
         public void Create(Project project)
         {
             SQLCall.CreateRequest<Project>(connectionString, "spCreateProject", project);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spCreateProject", con);
-                con.Open();
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Name", project.Name);
-                cmd.Parameters.AddWithValue("@ShortName", project.ShortName);
-                cmd.Parameters.AddWithValue("@Description", project.Description);
-                cmd.ExecuteNonQuery();
-            }*/
         }
 
         public Project GetById(int id)
         {
             return SQLCall.GetByIdRequest<Project>(connectionString, "spGetProjectById", id);
-            /*Project project = new Project();
-            bool isFind = false;
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand("spGetProjectById", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Id", id);
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    isFind = true;
-                    project.Id = Convert.ToInt32(rdr["Id"]);
-                    project.Name = rdr["Name"].ToString();
-                    project.ShortName = rdr["ShortName"].ToString();
-                    project.Description = rdr["Description"].ToString();
-                }
-                if (isFind)
-                {
-                    return project;
-                }
-                return null;
-                
-            }*/
         }
 
         public void Update(Project project)
         {
             SQLCall.UpdateRequest<Project>(connectionString, "spUpdateProject", project);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spUpdateProject", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@Id", project.Id);
-                cmd.Parameters.AddWithValue("@Name", project.Name);
-                cmd.Parameters.AddWithValue("@ShortName", project.ShortName);
-                cmd.Parameters.AddWithValue("@Description", project.Description);
-                cmd.ExecuteNonQuery();
-            }*/
         }
         public void Delete(int id)
         {
             SQLCall.DeleteRequest(connectionString, "spDeleteProjectById", id);
-            /*using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand("spDeleteProjectById", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@Id", id);
-                cmd.ExecuteNonQuery();
-            }*/
         }
     }
 }
